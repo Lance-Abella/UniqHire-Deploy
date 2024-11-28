4688f5d8d4ec7d9b137687e392a166004e5ed263
evrylsem <144195034+evrylsem@users.noreply.github.com>
2024-11-28 09:26:11 +0800

    try payment

--- resources/views/slugs/payment.blade.php
+++ resources/views/slugs/payment.blade.php
@@ -1,17 +1,23 @@
+<!-- <div id="donate-button-container">
+    <div id="donate-button"></div>
+    <script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></script>
+    <script>
+        PayPal.Donation.Button({
+            env: 'sandbox',
+            hosted_button_id: 'GMSJGCL9VRSZL',
+            image: {
+                src: 'https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif',
+                alt: 'Donate with PayPal button',
+                title: 'PayPal - The safer, easier way to pay online!',
+            }
+        }).render('#donate-button');
+    </script>
+</div> -->
 
-<div id="donate-button-container">
-<div id="donate-button"></div>
-<script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></script>
+<script src="https://www.paypal.com/sdk/js?client-id=BAA4NQ6v6VEbHbGBsmVdeIjPHrzoaCFsYF3NEvF3bCo8iExmlRJ8HE2HN9rLV2DFA-FxnLYN-3EcgXhTaU&components=hosted-buttons&disable-funding=venmo&currency=PHP"></script>
+<div id="paypal-container-ALJE4JSRH2YJW"></div>
 <script>
-PayPal.Donation.Button({
-env:'sandbox',
-hosted_button_id:'GMSJGCL9VRSZL',
-image: {
-src:'https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif',
-alt:'Donate with PayPal button',
-title:'PayPal - The safer, easier way to pay online!',
-}
-}).render('#donate-button');
+    paypal.HostedButtons({
+        hostedButtonId: "ALJE4JSRH2YJW",
+    }).render("#paypal-container-ALJE4JSRH2YJW")
 </script>
-</div>
-
