1bc5490a7260d2a5c59b86dbdf5a2c42e04ff3c1
pearkie <pearkieveern30@gmail.com>
2024-12-05 10:02:37 +0800

    Update 2024_07_24_024158_create_notifications_table.php

--- database/migrations/2024_07_24_024158_create_notifications_table.php
+++ database/migrations/2024_07_24_024158_create_notifications_table.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->boolean('read')->default(false);
             $table->uuid('id')->primary();
             $table->string('type');
             $table->morphs('notifiable');
             $table->text('data');
+            $table->boolean('read')->default(false);
             $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
